de4317c73aca4dd0faa4ce1f46bb3263f6189840
Seth Loh <lackdaz@gmail.com>
2018-10-07 21:12:40 +0800

    Add working monitor.py file

---
+++ flaskr/monitor.py
@@ -0,0 +1,32 @@
+import os
+import time
+import sqlite3
+
+import random
+import decimal
+
+
+def measure_temp():
+    temp = os.popen("vcgencmd measure_temp").readline()
+    return float(temp)
+    # return float(decimal.Decimal(random.randrange(0, 1000) / 10))
+
+
+def log_temp(temp):
+    conn = sqlite3.connect('./instance/flaskr.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        'INSERT INTO reading (temp) VALUES (?)',
+        (temp, )
+    )
+    print("logged")
+    conn.commit()
+    conn.close()
+
+def init():
+    while True:
+        time.sleep(5)
+        current_temp = measure_temp()
+        print("measured ", current_temp)
+        log_temp(current_temp)
